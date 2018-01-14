0faf01661a41f885f231c385562309b9df42f1c3
Andreas Grapentin <andreas@grapentin.org>
2018-01-14 20:24:53 +0100

    using a set instead of a list for scheduled checks

--- parabola_repolint/__main__.py
+++ parabola_repolint/__main__.py
@@ -27,7 +27,7 @@ def make_argparser():
     parser.add_argument('-X', '--noupdate', action='store_true', help='''
 do not attempt to update the repository and the chroots before linting
 ''')
-    parser.add_argument('-c', '--checks', type=lambda a: [s.strip() for s in a.split(',')],
+    parser.add_argument('-c', '--checks', type=lambda a: set(s.strip() for s in a.split(',')),
                         default=','.join(Linter.get_list_of_checks()), help='''
 comma-separated list of checks to perform. list of all known checks: %s
 ''' % ', '.join(Linter.get_list_of_checks()))
